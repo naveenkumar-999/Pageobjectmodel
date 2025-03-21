ce74b63e0df65bcd465e07b435a15a8bd97cfd2d
Naveen Kumar <naninaveenkumar279@gmail.com>
2025-03-21 18:29:08 +0530

    Update Sample.java

--- Pageobjectmodel/src/test/java/Sample.java
+++ Pageobjectmodel/src/test/java/Sample.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Sample {
 	
 	public static void main(String[] args) throws InterruptedException {
-		
-		System.setProperty("webdriver.chrome.driver","C:/Users/nanin/Downloads/chromedriver-win64 (10)/chromedriver-win64/chromedriver.exe");
+
+		if(driver == null)
+		{
+	        System.setProperty("webdriver.chrome.driver","C:/Users/nanin/Downloads/chromedriver-win64 (10)/chromedriver-win64/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
-	    Thread.sleep(2000);
-	    driver.close();
-	    
+	        Thread.sleep(2000);
+	        driver.close();
+		}
 		
 		}
 	
